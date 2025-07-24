1044f77fc253b5a2891897d937b97609e3ed0341
Nayan <dreamerboynayan@gmail.com>
2025-07-24 21:44:26 +0600

    JS problem solve with functions

---
+++ function.js
@@ -0,0 +1,57 @@
+// Problem 1: Write a function to find the sum of the father's and son's ages.
+
+function sonFatherAgeSum (fAge, sAge){
+    let sum = fAge + sAge;
+    console.log(sum);
+}
+
+sonFatherAgeSum(40, 15);
+
+
+// Problem 2: Write a function to add two numbers.
+
+function add2num (num1, num2){
+    let sum = num1 + num2;
+    console.log(sum);
+}
+
+add2num(100, 50);
+
+// Problem 3: Write a function to square a number.
+
+function square(num){
+    let sqr = num * num;
+    console.log(sqr)
+}
+
+square(5);
+
+// Problem 4:  Write a function to subtract two numbers.
+
+function subtract2num (num1, num2){
+    let subtract = num1 - num2;
+    console.log(subtract);
+}
+
+subtract2num(100, 50);
+
+
+// Problem 5:  Write a function to multiply two numbers.
+
+function multiply2num (num1, num2){
+    let multiply = num1 * num2;
+    console.log(multiply);
+}
+
+multiply2num(100, 50);
+
+
+// Problem 6:  Write a function to divide two numbers.
+
+function divide2num (num1, num2){
+    let divide = num1 / num2;
+    console.log(divide);
+}
+
+divide2num(100, 50);
+
